eab89c32d8397544ae13c85cb47928d5d4009bd7
Shannon Sands <shannon.sands.1979@gmail.com>
2025-05-18 13:18:40 -0700

    Testing new branch

--- atroposlib/envs/base.py
+++ atroposlib/envs/base.py
@@ -815,6 +815,7 @@ class BaseEnv(ABC):
                 await self.wandb_log({})
 
     async def add_train_workers(self):
+        logger.info(f"Adding train workers, max_num_workers: {self.max_num_workers}")
         if (self.eval_runner is not None) and (not self.eval_runner.done()):
             if self.config.eval_handling == EvalHandlingEnum.STOP_TRAIN:
                 return
