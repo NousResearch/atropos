6c2f1ac408c772d691176c0c05e51f07df0ace62
Wind <116412564+windlgrass@users.noreply.github.com>
2026-01-29 15:16:05 +0700

    Update dataset_env.py

--- environments/dataset_environment/dataset_env.py
+++ environments/dataset_environment/dataset_env.py
@@ -78,7 +78,6 @@ class DatasetEnv(BaseEnv):
         self.metric_buffer = {}
 
         self.reward_function = self._initialize_reward_function()
-        self.current_item = None
 
     def _initialize_reward_function(self):
         if hasattr(self.config, "reward_functions") and self.config.reward_functions:
